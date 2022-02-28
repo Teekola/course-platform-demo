54d81f51aeaff2ce9d9ae41415d841ddc363e4e3
Teemu Kerkola <tokerk@utu.fi>
2022-03-01 00:00:52 +0200

    added TODO

--- pages/api/auth/signIn.js
+++ pages/api/auth/signIn.js
@@ -3,11 +3,12 @@ export default function handler(req, res) {
     const { username, password } = req.body;
 
     // TODO:
-    // 1) Add salt
+    // 1) Fetch from the DB the hash and the salt matching the username
+    // 2) Add the salt to the password
     // Repeat until iterated through all peppers or match
-    //      2) pick a pepper and add to the password
-    //      3) get the hashvalue
-    //      4) compare to the DB hashvalue
+    //      3) pick a pepper and add to the password
+    //      4) get the hashvalue
+    //      5) compare to the DB hashvalue
     if (username === "johndoe" && password === "123") {
         res.status(200).json({ id: 1, name: 'John Doe' });
     } else {
